c0891bc79e3c0b2c859c7b82f928f0e4c4311e1c
rojandahal <maxpanye60@gmail.com>
2020-03-23 08:55:35 +0545

    Maps including_2

--- Collection/src/com/company/Map.java
+++ Collection/src/com/company/Map.java
@@ -13,6 +13,26 @@ public class Map {
         languages.put("BASIC","Beginners All purpose symbolic instruction code");
 
         System.out.println(languages.get("Java"));
+        System.out.println(languages.put("Java", "I am learning java"));  // This will overwrite the Value for Java key but prints the
+        // previous value and when we print it again the new value gets printed
+        System.out.println(languages.get("Java"));  //This will print the overwritten value
+
+        //These lines below are used to check whether a key is present or not, when the key is present then it is checked by
+        //languages.containsKey()
+        if (languages.containsKey("Kotlin")){
+            System.out.println("Kotlin is already present");
+        }else
+            languages.put("Kotlin", "Kotlin is love");
+        System.out.println(languages.get("Kotlin"));
+
+        System.out.println("==============================================================");
+        //This languages.keySet() returns the sets of key of the maps and when we loop through all of them which prints the key and
+        //the values of the keys
+        for(String key: languages.keySet())
+        {
+            System.out.println(key + " : " + languages.get(key));
+        }
+
 
 
     }
